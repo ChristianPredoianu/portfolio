f7c1fd5edea08d8edb4c7860945beb4b1809dcc3
Christian Predoianu <christianpredoianu@yahoo.com>
2022-05-07 13:31:46 +0200

    work on portfolio

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
+const hamburger = document.getElementById('hamburger');
 
+document.getElementById('hamburger').addEventListener('click', () => {
+  const navLinks = document.getElementById('nav-links');
+  navLinks.classList.toggle('open');
+  console.log(navLinks.classList);
+});
