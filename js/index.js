c3fe9816c655311e3a268eb386169d129684914f
Christian Predoianu <christianpredoianu@yahoo.com>
2022-05-31 18:01:59 +0200

    deleted

--- js/index.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-
-document.getElementById('hamburger').addEventListener('click', () => {
-  const navLinks = document.getElementById('nav-links');
-  navLinks.classList.toggle('open');
-  console.log(navLinks.classList);
-});
